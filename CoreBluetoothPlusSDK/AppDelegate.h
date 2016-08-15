731e0fe89bf62d68b44d39eea175587dc44e70aa
huangxiong <huangxionging@163.com>
2016-08-15 15:18:56 +0800

    Initial Commit

---
+++ CoreBluetoothPlusSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreBluetoothPlusSDK
+//
+//  Created by huangxiong on 16/8/15.
+//  Copyright © 2016年 huangxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
